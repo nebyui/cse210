4a2f464f3ffd72e42d8743056326341fdf576afe
StrayStrider <StrayStrider@users.noreply.github.com>
2024-01-21 00:52:04 -0700

    Did programming prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+        int guess;
+
+        do
+        {
+            System.Console.Write("Guess a number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > randomNumber)
+            {
+                System.Console.WriteLine("Too High");
+            }
+
+            else if (guess < randomNumber)
+            {
+                System.Console.WriteLine("Too Low");
+            }
+
+            else
+            {
+                System.Console.Write("Correct");
+            }
+        } while (guess != randomNumber);
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int newNumber = 1;
+        int sum = 0;
+        float totalItems = 0;
+        int largestNumber = 0;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers. Enter 0 when done.");
+
+        while (newNumber != 0)
+        {
+            System.Console.Write("Enter Number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        };
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            totalItems += 1;  // Sample Solution used numbers.Count to get number of items when calculating average//
+            if (largestNumber < number)
+            {
+                largestNumber = number;
+            }
+
+        };
+
+        float average = sum / totalItems;
+        System.Console.Write("Sum: ");    // could be same lines: System.Console.Write("Sum: {sum}")
+        System.Console.WriteLine(sum);
+        System.Console.Write("Average: ");
+        System.Console.WriteLine(average);
+        System.Console.Write("Largest Number: ");
+        System.Console.WriteLine(largestNumber);
+
+
+
+
+
     }
 }
