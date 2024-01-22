45af96592b32ebe245994c3b18a6b6b619c2c6e4
StrayStrider <StrayStrider@users.noreply.github.com>
2024-01-22 12:56:49 -0700

    Learning 2 Activity

---
+++ prepare/Learning02/Person.cs
@@ -0,0 +1,10 @@
+class Person
+{
+    public string name;
+    public string phone;
+
+    public string DisplayString()
+    {
+        return $"{name} {phone}";
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        int count = 23;
+
+        var cars = new List<Car>();
+
+        var car = new Car();
+        car.make = "Honda";
+        car.model = "Civic";
+        car.gallons = 10;
+        car.milesPerGallon = 30;
+
+        var owner = new Person();
+        owner.name = "bob";
+        owner.phone = "333-333-3333";
+        car.owner = owner;
+
+
+        cars.Add(car);
+
+        car = new Car();
+        car.make = "Ford";
+        car.model = "F- 150";
+        car.year = 2023;
+        car.gallons = 30;
+        car.milesPerGallon = 5;
+
+        owner = new Person();
+        owner.name = "Sue";
+        owner.phone = "444-444-4444";
+        car.owner = owner;
+
+        cars.Add(car);
+
+        Console.Clear();
+        foreach (var c in cars)
+        {
+            // System.Console.WriteLine($"{c.make} {c.model}: totalRange = {c.TotalRange()}");
+            c.Display();
+        }
     }
 }
