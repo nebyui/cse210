da30bfb2015bca5193afa2328735892b6d9a7b89
StrayStrider <StrayStrider@users.noreply.github.com>
2024-02-22 21:49:37 -0700

    w07 learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        string summary1 = assignment1.GetSummary();
+        Console.WriteLine(summary1);
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        string summary2 = assignment2.GetSummary();
+        string mathSummary = assignment2.GetHomeworkList();
+        Console.WriteLine(summary2);
+        Console.WriteLine(mathSummary);
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        string summary3 = assignment3.GetSummary();
+        string writingSummary = assignment3.GetWrtingInformation();
+        Console.WriteLine(summary3);
+        Console.WriteLine(writingSummary);
     }
 }
