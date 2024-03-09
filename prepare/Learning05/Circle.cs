e1f0c4607a03df2619c6196d083977dee096314a
StrayStrider <StrayStrider@users.noreply.github.com>
2024-03-08 19:03:12 -0700

    shape program

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,16 @@
+class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+
+    }
+
+    public override double GetArea()
+    {
+        double area = Math.PI * Math.Pow(_radius, 2);
+        return area;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle("blue", 8.5, 7.2);
+        Circle circle = new Circle("green", 3.14);
+
+        shapeList.Add(square);
+        shapeList.Add(rectangle);
+        shapeList.Add(circle);
+
+        foreach (Shape shape in shapeList)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"{color} - {area}");
+        }
+
+
+
     }
 }
